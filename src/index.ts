9ab2c8ce6c9406f7633952ff17af6cbb54e7c5b0
Anil Oli aniloli <aniloli@txstate.edu>
2025-10-26 13:17:39 -0500

    add SemesterManager, handle loadSemesterData

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,4 @@
-async function loadSemesterData() {
-  try {
-    const semester = getSemester();
+import { SemesterManager } from "./modules/SemesterManager";
 
-    if (semester == undefined) throw new Error("unable to get semester");
-
-    const semesterFile = await import(`./datas/${semester}.js`).then(
-      (data) => data.default
-    );
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(`Error during exectuion: ${error.message}`);
-    }
-  }
-}
-
-loadSemesterData();
-
-function getSemester() {
-  const websiteURL = new URL(location.href);
-  const urlPath = websiteURL.pathname.split("/").filter(Boolean).pop(); // splitting url segments
-
-  if (urlPath == undefined) throw new Error("unable to process URL");
-
-  const firstSegment = urlPath.split(".").at(0); // split into . to remove extension and get first element
-  const semester = firstSegment?.split("-").at(0); // split the section segment to get specific word by splitting using "-"
-
-  return semester;
-}
+const semesterManager = new SemesterManager();
+semesterManager.loadSemesterData();
